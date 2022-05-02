b763f1df29fc2869b5a8ff2dae758e6f286b69e3
Eugene Petrushevskiy <odessa.eugene@gmail.com>
2022-05-02 17:28:45 +0300

    comit 7

--- src/App.java
+++ src/App.java
@@ -1,14 +1,5 @@
 public class App {
-    public static void main(String[] args) {
-        try {
-            System.err.print(" 0");
-            if (true) {throw new Error();}
-            System.err.print(" 1");
-        } catch(Error e) {
-            System.err.print(" 2");
-        } finally {
-            System.err.print(" 3");
-        }
-        System.err.print(" 4");
+    public static void main(String[] args) throws Exception { // предупреждаем о Exception
+        throw new Exception(); // и кидаем Exception
     }
 }
